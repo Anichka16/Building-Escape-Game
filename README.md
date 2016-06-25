73aaaf8049e31f10448849655f3b8e4a4f615a55
Anastasiya Hristova <anastasiya.hristova19@gmail.com>
2016-06-25 15:28:32 +0300

    First commit Building escape game Section 3, 86

--- README.md
+++
@@ -1,2 +0,0 @@
-# Building-Escape-Game
-Unreal Engine Game
---
+++ Source/BuildingEscapeA/BuildingEscapeAGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BuildingEscapeAGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPEA_API ABuildingEscapeAGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
